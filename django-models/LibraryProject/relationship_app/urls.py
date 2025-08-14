eeb758086355dbbcaf278936d57c5475156bd5d8
Peter Quayetey <52321254+petertyt@users.noreply.github.com>
2025-08-14 01:39:04 +0000

    Add authentication views and registration functionality in relationship_app; update URL patterns to include login, logout, and register routes.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
+from django.contrib.auth import views as auth_views
+from .views import register
 
 
 urlpatterns = [
 	path("books/", list_books, name="list_books"),
 	path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+	# Auth
+	path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+	path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+	path("register/", register, name="register"),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 def list_books(request: HttpRequest) -> HttpResponse:
@@ -22,3 +24,15 @@ class LibraryDetailView(DetailView):
 
 	def get_object(self):
 		return get_object_or_404(Library.objects.prefetch_related("books__author"), pk=self.kwargs["pk"])
+
+
+def register(request: HttpRequest) -> HttpResponse:
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("login")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "relationship_app/register.html", {"form": form})
