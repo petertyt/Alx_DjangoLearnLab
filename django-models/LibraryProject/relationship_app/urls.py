293a663ffa5773b5bd5669a2ceed2528c75ab9c7
Peter Quayetey <52321254+petertyt@users.noreply.github.com>
2025-08-14 01:40:48 +0000

    Refactor URL patterns in relationship_app to use views module for registration route, improving code clarity and consistency.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 from django.contrib.auth import views as auth_views
-from .views import register
+from . import views
 
 
 urlpatterns = [
@@ -11,6 +11,6 @@ urlpatterns = [
 	# Auth
 	path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
 	path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-	path("register/", register, name="register"),
+	path("register/", views.register, name="register"),
 ]
 
