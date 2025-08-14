79564ddf489a33eefa41ad8abb4923bc4fe9cced
Peter Quayetey <52321254+petertyt@users.noreply.github.com>
2025-08-14 02:12:52 +0000

    Add book list view with permission checks in bookshelf app; implement view to render all books with access control for authorized users.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request: HttpRequest) -> HttpResponse:
+	books = Book.objects.all()
+	return render(request, 'bookshelf/book_list.html', {'books': books})
