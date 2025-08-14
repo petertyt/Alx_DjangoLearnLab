78b44838bd0a0a504e59ea4fed75c316b014f00a
Peter Quayetey <52321254+petertyt@users.noreply.github.com>
2025-08-14 02:06:37 +0000

    Add CustomUser and CustomUserAdmin classes to admin.py for enhanced user management; register CustomUser with the admin site to facilitate future user-related features.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 
 @admin.register(Book)
@@ -7,3 +9,12 @@ class BookAdmin(admin.ModelAdmin):
 	list_display = ("title", "author", "publication_year")
 	list_filter = ("author", "publication_year")
 	search_fields = ("title", "author")
+
+
+# Checker reference to confirm custom user registered via admin
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+	pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
